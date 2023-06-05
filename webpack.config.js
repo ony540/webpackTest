6ed5047268ce6f4f08c6ce8556bbf50780d6cf60
NAYEONG OH <102240503+ony540@users.noreply.github.com>
2023-06-05 14:33:10 +0900

    bannerPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require("path"); //운영체제별로 상이한 경로 문법을 해결해 절대 경로로 반환하는 역할을 합니다.
 
-const webpack = require("webpack");
+const webpack = require("webpack"); //배너 달기위해서 웹팩 모듈
+
+const childProcess = require("child_process"); //터미널 명령어를 웹팩에서도 사용할 수 있도록
 
 module.exports = {
     mode: "development",
@@ -43,7 +45,12 @@ module.exports = {
     plugins: [
         new webpack.BannerPlugin({
             //toLocaleString : 날짜의 문자열 표현을 지역의 언어에 맞는 형식으로 반환합니다.
-            banner: "마지막 빌드 시간은 " + new Date().toLocaleString() + " 입니다.",
+            // banner: "마지막 빌드 시간은 " + new Date().toLocaleString() + " 입니다.",
+            banner: `
+            Commit version : ${childProcess.execSync("git rev-parse --short HEAD")}
+            Committer : ${childProcess.execSync("git config user.name")}
+            Commit Date : 마지막 빌드 시간은 ${new Date().toLocaleString()} 입니다
+            `,
         }),
     ],
 };
